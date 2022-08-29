bc0f0a84d2458638b317c860095a4bd183d3c5c7
golosin <mail325@mail.ru>
2022-08-29 21:03:44 +0300

    test fix

--- tools_function/tools.py
+++ tools_function/tools.py
@@ -9,10 +9,8 @@ def get_url():
 
 # SingIn (авторизация и получение токена)
 def get_singIn():
-    # email = os.environ['EMAIL_SECRET']
-    # password = os.environ['PASSWORD_SECRET']
-    email = 'testadmin@t4mp.com'
-    password = '9hm2wAg8rDzP2kn'
+    email = os.environ['EMAIL_SECRET']
+    password = os.environ['PASSWORD_SECRET']
 
     x = open(os.path.join(r"query", "mutation SignInMutation.txt"))
     body = ''.join(x)
